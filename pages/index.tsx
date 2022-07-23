48fbc3782462df7443c0e989cdfe4387472ce421
abdelmawla <abdelmawla.souat@gmail.com>
2022-07-24 00:52:30 +0200

    Init the repo

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const popo = "fdpff";
+
+  console.log("popo");
+
+  return <div className={styles.container}></div>;
+};
+
+export default Home;
